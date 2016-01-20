3acb25abf2dda31896c7b77b1bfad48d16447645
lanzhiping <lannano@qq.com>
2016-01-20 18:02:40 +0800

    project init
    project init

---
+++ gruntfile.js
@@ -0,0 +1,28 @@
+﻿module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		watch: {
+			files: ['**/*.js'],
+			//tasks: ['jshint']
+		},
+
+		iisexpress: {
+			server: {
+				options: {
+					port: 3000,
+					open: true,
+					openPath: '/backbone/index.html'
+				}
+			}
+		}
+	});
+
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-iisexpress');
+
+	grunt.registerTask('default', ['iisexpress', 'watch']);
+
+};
