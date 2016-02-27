a25485ff88a730ec77e1a7e36825a9d817ed9960
zhiping lan <lannano@qq.com>
2016-02-27 13:07:42 +0800

    refactor initDatabase

--- InitDataBase.js
+++ InitDataBase.js
@@ -1,75 +1,89 @@
-module.exports = function InitDataBase(filename) {
+'use strict';
 
-	var fs = require('fs'),
-		filePath = './db/'+ filename + '.js';
+var fs = require('fs');
 
-	function compare(a, b) {
-		if (a.id < b.id) return -1;
-		if (a.id > b.id) return 1;
-		return 0;
-	}
+function compare(a, b) {
+    if (a.id < b.id) return -1;
+    if (a.id > b.id) return 1;
+    return 0;
+}
 
-	try{
-		var stats = fs.statSync(filePath);
-		stats.isDirectory() && (function(){throw '"' + filename + '" is not vaild.';})();
+function validateFilePath(filename) {
+    var filePath = './db/' + filename + '.js';
+    try {
+        var stats = fs.statSync(filePath);
+        if (stats.isDirectory()) {
+            throw '{0} is not a valid file.'.replace('{0}', filename);
+        }
 
-		this.read = function (objectType) {
-			console.log('read: ' + (objectType || 'all'));
-			var fileContent = fs.readFileSync(filePath, 'utf8');
-			var	rootObject = fileContent.length == 0 ? {} : JSON.parse(fileContent);
-			return objectType ? rootObject[objectType] : rootObject;
-		};
+    } catch (e) {
+        console.log(e);
+    }
 
-		this.deleteByIdAndType = function(objectType, id){
-			console.log('delete type:' + objectType + ' id:' + id);
-			var rootObject = this.read(),
-				updateObjectSet = rootObject[objectType];
+    return filePath;
+}
 
-			if(!updateObjectSet){
-				console.log('objects of ' +objectType+' doesnt exist.');
-				return false;
-			}
-			var deleteObject = updateObjectSet.filter(function(item){return item.id === id});
-			if (!deleteObject) {
-				console.log(
-					'the object type:{0} id:{1} doesnt exist.'
-					.replace('{0}', objectType)
-					.replace('{1}',id));
-				return false;
-			};
+function InitDataBase(filename) {
 
-			rootObject[objectType] = updateObjectSet.filter(function(item){return item.id != id});
-			fs.writeFileSync(filePath, JSON.stringify(rootObject));
-			return true;
-		};
+    this.filePath = validateFilePath(filename);
 
-		this.write = function (objectType, obj) {
-			console.log('write: ' + objectType);
+    this.read = function (objectType) {
+        console.log('read: ' + (objectType || 'all'));
 
-			var rootObject = this.read(); // todo: should be implement when init
-			if(rootObject[objectType] == undefined) rootObject[objectType] = [];
+        var fileContent = fs.readFileSync(this.filePath, 'utf8');
+        var rootObject = fileContent.length == 0 ? {} : JSON.parse(fileContent);
+        return objectType ? rootObject[objectType] : rootObject;
+    };
 
-			if(obj.id != undefined) {
-				rootObject[objectType].forEach(function(item, index){ 
-					if( item.id === obj.id )
-					rootObject[objectType][index] = obj;
-					return ;
-				});
-			} else {
-				var hightestContinousElement = rootObject[objectType]
-											.sort(compare)
-											.filter(function(item, index){ return item.id == index})
-											.pop();
+    this.deleteByIdAndType = function (objectType, id) {
+        console.log('delete type:' + objectType + ' id:' + id);
 
-				obj.id = hightestContinousElement && rootObject[objectType].sort(compare)[0].id === 0
-					   ? hightestContinousElement.id + 1 : 0;
-				rootObject[objectType].push(obj);
-			}
-			
-			fs.writeFileSync(filePath, JSON.stringify(rootObject));
-		};
+        var rootObject = this.read(),
+            updateObjectSet = rootObject[objectType];
 
-	}catch(e){
-		throw '"' + filename + '" is not vaild.';
-	}
+        if (!updateObjectSet) {
+            console.log('objects of ' + objectType + ' doesnt exist.');
+            return false;
+        }
+        var deleteObject = updateObjectSet.filter(function (item) { return item.id === id });
+        if (!deleteObject) {
+            console.log(
+                'the object type:{0} id:{1} doesnt exist.'
+                .replace('{0}', objectType)
+                .replace('{1}', id));
+            return false;
+        };
+
+        rootObject[objectType] = updateObjectSet.filter(function (item) { return item.id != id });
+        fs.writeFileSync(this.filePath, JSON.stringify(rootObject));
+        return true;
+    };
+
+    this.write = function (objectType, obj) {
+        console.log('write: ' + objectType);
+
+        var rootObject = this.read();
+        if (rootObject[objectType] == undefined) rootObject[objectType] = [];
+
+        if (obj.id != undefined) {
+            rootObject[objectType].forEach(function (item, index) {
+                if (item.id === obj.id)
+                    rootObject[objectType][index] = obj;
+                return;
+            });
+        } else {
+            var hightestContinousElement = rootObject[objectType]
+                                        .sort(compare)
+                                        .filter(function (item, index) { return item.id == index })
+                                        .pop();
+
+            obj.id = hightestContinousElement && rootObject[objectType].sort(compare)[0].id === 0
+                   ? hightestContinousElement.id + 1 : 0;
+            rootObject[objectType].push(obj);
+        }
+
+        fs.writeFileSync(this.filePath, JSON.stringify(rootObject));
+    };
 };
+
+module.exports = InitDataBase;
