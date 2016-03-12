2c499c9fb2cf0d45ca77709992df64d403d02df7
zhiping lan <lannano@qq.com>
2016-03-12 09:54:57 +0800

    refactor backbone service

--- bootstrap.js
+++ bootstrap.js
@@ -1,19 +1,25 @@
 ﻿'use strict';
 
 var port = 8000,
+    bodyParser = require("body-parser"),
     serverBuilder = require('./server/serverBuilder');
 
-
-(function() {
-
+(function () {
     var express = require('express'),
-        app = express();
+        app = express(),
+        backboneServer = new require("./server/backboneService").build("test");
+
+    app.get("/", express.static("backbone"));
 
     app.use("/dist", express.static("dist"));
     app.use("/font", express.static("font"));
     app.use("/backbone", express.static("backbone"));
-    app.get("/", express.static("backbone"));
-    app.all("/api/backbone", require("./server/backboneServer")("test"));
+
+    app.use(bodyParser.json());
+    app.get("/api/backbone", backboneServer.get);
+    app.post("/api/backbone", backboneServer.post);
+    app.put("/api/backbone", backboneServer.put);
+    app.delete("/api/backbone", backboneServer.delete);
 
     serverBuilder
         .withPort(process.env.PORT || port)
